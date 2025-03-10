68b0f10b61d7266c59acc1ca17327135db09f21d
eric <152508139+e2392580@users.noreply.github.com>
2025-03-10 15:26:55 -0400

    s8c1 Section hero and footer

--- functions.php
+++ functions.php
@@ -1,5 +1,54 @@
 <?php
 
+function theme_tp_customize_register($wp_customize) {
+    // Le code pour ajouter des sections, des réglages et des contrôles ira ici.
+    // Création d'une nouvelle section dans le customizer
+    $wp_customize->add_section('hero_section', array(
+        'title' => __('Section hero', 'theme_tp'),
+        'priority' => 30,
+    ));
+    //////////////////////  ajout de la donnée
+    $wp_customize->add_setting('hero_auteur', array(
+        'default' => __('Eric Vu', 'theme_tp'),
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    ///////////////////// ajout du contrôle de la donnée
+    $wp_customize->add_control('hero_auteur', array(
+        'label' => __('Auteur', 'theme_tp'),
+        'section' => 'hero_section',
+        'type' => 'text',
+    ));  
+    ///////////////////// Ajout de la donnée  image en background
+    $wp_customize->add_setting('hero_background', array(
+        'default' => '',
+        'sanitize_callback' => 'esc_url_raw',
+    ));
+    ///////////////////// Ajout du contrôle de la donnée
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_background', array(
+        'label' => __('Image en arrière plan', 'theme_tp'),
+        'section' => 'hero_section',
+    )));
+    //////////////////// Ajout 
+    $wp_customize->add_section('footer_section', array(
+        'title' => __('Section footer', 'theme_tp'),
+        'priority' => 30,
+    ));
+    //////////////////////  ajout de la donnée
+    $wp_customize->add_setting('footer_adresse', array(
+        'default' => __('5800 Sherbrooke-est - Montréal (Québec) H1X 2A2', 'theme_tp'),
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    ///////////////////// ajout du contrôle de la donnée
+    $wp_customize->add_control('hero_auteur', array(
+        'label' => __('Adresse', 'theme_tp'),
+        'section' => 'footer_section',
+        'type' => 'text',
+    ));  
+
+}
+
+add_action('customize_register', 'theme_tp_customize_register');
+
 function mon_theme_supports() {
     add_theme_support('title-tag');
     add_theme_support('menus');
