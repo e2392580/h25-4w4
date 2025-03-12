703195ab82eb75d4905f7b19163227d2b0ca8731
eric <152508139+e2392580@users.noreply.github.com>
2025-03-12 17:31:31 -0400

    s8c2 Avancement du tp1

--- footer.php
+++ footer.php
@@ -5,15 +5,17 @@ $footer_telephone = get_theme_mod('footer_telephone', '(514-254-7131)');
 $footer_mission = get_theme_mod('footer_mission', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, nostrum sint deserunt architecto dolorem nisi delectus pariatur odit eius! Maiores dolore provident soluta culpa minus nesciunt doloremque vero incidunt accusamus!'); 
 ?>
 
-  <div class="piedpage global">
+  <div class="piedpage">
     <section class="piedpage__s1">
       <div class="piedpage__s1__externe">
+      <h2>Liens sur les voyages</h2>
         <?php wp_nav_menu(array(
           "menu" => "externe",
           "container" => "nav",
         )); ?>
       </div>
       <div class="piedpage__s1__adresse">
+        <h2> Nos coordinations</h2>
         <div class="piedpage__s1_adresse__coord">
         <p class="footer_adresse"><?php echo $footer_adresse ?></p>
         <p class="footer_telephone"><?php echo $footer_telephone ?></p>
