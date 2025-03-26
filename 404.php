6706a8193bbd398806f2284163a2059d0ac3c54d
e2392580 <e2392580@cmaisonneuve.qc.ca>
2025-03-25 23:26:21 -0400

    s9c1 Erreur 404 ameliorer

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 <section class="erreur">
-    <h1>OOPS!</h1>
-    <h2>Page non retrouvée</h2>
-    <p><a href="<?php echo get_home_url(); ?>"  class="button__erreur">Retourner à l'accueil</a></p>
+    <h1>404</h1>
+    <h2>Oups ! Page introuvable</h2>
+    <p>Il semble que la page que vous recherchez n'existe pas ou a été déplacée.</p>
+    <img src="<?php echo get_template_directory_uri(); ?>/images/404.png" alt="Erreur 404">
+    <p><a href="<?php echo get_home_url(); ?>" class="button__erreur">Retourner à l'accueil</a></p>
 </section>
-    <?php get_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
--- functions/customizer.php
+++ functions/customizer.php
@@ -78,14 +78,14 @@ function theme_tp_customize_register($wp_customize) {
     )));
 
     
-    $wp_customize->add_setting('hero_background', array(
+    $wp_customize->add_setting('erreur_background', array(
         'default' => '',
         'sanitize_callback' => 'esc_url_raw',
     ));
     ///////////////////// Ajout du contrôle de la donnée
-    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_background', array(
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'erreur_background', array(
         'label' => __('Image en arrière plan', 'theme_tp'),
-        'section' => 'hero_section',
+        'section' => 'erreur_section',
     )));
 }
 
